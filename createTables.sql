81c9860b935d12414fe931b2af6626d60dc60564
megaurso <bernadogui@hotmail.com>
2023-02-13 13:45:06 -0300

    fix createTables

--- createTables.sql
+++ createTables.sql
@@ -1,7 +1,4 @@
-CREATE DATABASE "projectDevelopers"
-
-
-CREATE TYPE "OS" AS ENUM ('Windows', 'Linux', 'MacOS');
+CREATE DATABASE "projectDevelopers" CREATE TYPE "OS" AS ENUM ('Windows', 'Linux', 'MacOS');
 
 CREATE TABLE IF NOT EXISTS developer_infos (
 	"id" BIGSERIAL PRIMARY KEY,
@@ -10,31 +7,70 @@ CREATE TABLE IF NOT EXISTS developer_infos (
 );
 
 CREATE TABLE IF NOT EXISTS developers (
-	id BIGSERIAL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	email VARCHAR(50) NOT NULL UNIQUE,
-	developerInfoId INTEGER UNIQUE chave estrangeira
+	"id" BIGSERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	"email" VARCHAR(50) NOT NULL UNIQUE,
+	"developerInfoId" INTEGER UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS projects (
-	id BIGSERIAL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL,
-	description TEXT NOT NULL,
+	"id" BIGSERIAL PRIMARY KEY,
+	"name" VARCHAR(50) NOT NULL,
+	"description" TEXT NOT NULL,
 	"estimatedTime" VARCHAR(20) NOT NULL,
-	repository VARCHAR(120) NOT NULL,
+	"repository" VARCHAR(120) NOT NULL,
 	"startDate" DATE NOT NULL,
 	"endDate" DATE,
-	"developerId" INTEGER NOT NULL chave estrangeira.
+	"developerId" INTEGER NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS technologics (
-	id BIGSERIAL PRIMARY KEY,
-	name VARCHAR(30) NOT NULL
+	"id" BIGSERIAL PRIMARY KEY,
+	"name" VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS projects_technologies (
-	id BIGSERIAL PRIMARY KEY,
-	addedIn DATE NOT NULL SET DEFAULT now(),
-	email INTEGER NOT NULL chave estrangeira,
-	developerInfoId INTEGER NOT NULL chave estrangeria
+	"id" BIGSERIAL PRIMARY KEY,
+	"addedIn" DATE NOT NULL DEFAULT now(),
+	"projectId" INTEGER NOT NULL,
+	"technologyId" INTEGER NOT NULL
 );
+
+INSERT INTO
+	technologics (id, name)
+VALUES
+	(1, 'JavaScript'),
+	(2, 'Python'),
+	(3, 'React'),
+	(4, 'Express.js'),
+	(5, 'HTML'),
+	(6, 'CSS'),
+	(7, 'Django'),
+	(8, 'PostgreSQL'),
+	(9, 'PostgreSQL');
+
+	
+ALTER TABLE
+	developers
+ADD
+	FOREIGN KEY ("developerInfoId") REFERENCES developer_infos("id") ON DELETE CASCADE;
+
+ALTER TABLE
+	projects
+ADD
+	FOREIGN KEY ("developerId") REFERENCES developers("id") ON DELETE CASCADE;
+
+ALTER TABLE
+	projects_technologies
+ADD
+	FOREIGN KEY ("projectId") REFERENCES projects("id");
+
+ALTER TABLE
+	projects_technologies
+ADD
+	FOREIGN KEY ("technologyId") REFERENCES technologies("id");
+
+ALTER TABLE
+	projects_technologies
+ADD
+	FOREIGN KEY ("technologyId") REFERENCES technologics("id");
